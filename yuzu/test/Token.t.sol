ba188192ea47917d8c1ebf34a1faa42ff0eded3a
MoD <mod@tail.cat>
2025-04-05 23:33:34 +0800

    Adding yuzu token

---
+++ yuzu/test/Token.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {YuzuToken} from "../src/Token.sol";
+
+contract TokenTest is Test {
+    YuzuToken public token;
+    address public owner = address(0x1);
+    uint256 public initialSupply = 420_000_000e18;
+
+    function setUp() public {
+        token = new YuzuToken(owner, block.chainid);
+    }
+
+    function testMetadata() public {
+        assertEq(token.name(), "Yuzu");
+        assertEq(token.symbol(), "YUZU");
+    }
+
+    function testInitialSupply() public {
+        assertEq(token.balanceOf(owner), initialSupply);
+        assertEq(token.totalSupply(), initialSupply);
+    }
+
+    function testNoInitialSupplyOnDifferentChain() public {
+        uint256 differentChainId = block.chainid + 1;
+        YuzuToken noSupplyToken = new YuzuToken(owner, differentChainId);
+        
+        assertEq(noSupplyToken.balanceOf(owner), 0);
+        assertEq(noSupplyToken.totalSupply(), 0);
+    }
+}
